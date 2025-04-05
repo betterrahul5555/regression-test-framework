74a5b0c4fff043105f4d7d57b9cb2abd68dc1fc6
betterrahul5555 <63734203+betterrahul5555@users.noreply.github.com>
2025-04-05 14:40:40 +0200

    mistake

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,6 @@
 def test_login_valid_user():
   username = "admin"
   password = "admin123"
-  assert username == "admin" and passowrd == "admin123"
+  assert username == "admin" and password == "admin123"
 
 
